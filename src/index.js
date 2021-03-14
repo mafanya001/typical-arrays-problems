514020b5d1f89ba9117e672f8701f2b391ead899
mafanya001 <79634135+mafanya001@users.noreply.github.com>
2021-03-14 12:19:57 +0300

    fix: add working solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array[0]=== undefined) {
+         return 0;
+    };
+    return Math.min.apply(null, array); 
 }
 
+    
+  
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array[0]=== undefined) {
+        return 0;
+    };
+    return Math.max.apply(null, array); 
 }
+ 
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array[0]=== undefined) {
+        return 0;
+    };   
+    
+     let avg = 0;
+        for(var i=0; i<array.length; i++) {
+        avg += array[i];
+    };
+        
+    avg = avg/array.length;
+        return avg; 
 }
